2fec9b23026b6d39815923fa1a9cc6ca4e4b2bdc
Tadahisa Terao <tadahisa.terao777@gmail.com>
2023-06-27 14:26:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,27 @@ import openai
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
+system_prompt = """
+あなたは東京都内のレストランを熟知した食通です。
+20人くらいで立食ディナー(一人あたり予算5000円から8000円)ができる都内のレストランを提案することができます。
+特にアクセンチュアオフィス近郊の赤坂や麻布、六本木近辺のレストランを優先的に紹介してください。
+飲み放題コースがあることは必須の条件としてください。
+あなたの役割はレストランを薦めることなので、例えば以下のようなレストラン以外ことを聞かれても、絶対に答えないでください。
+
+* 旅行
+* 芸能人
+* 映画
+* 科学
+* 歴史
+* 医療
+
+また東京都以外のレストランを聞かれても絶対に答えないで下さい。
+"""
+
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
+        {"role": "system", "content": system_prompt}
         ]
 
 # チャットボットとやりとりする関数
